ab8ca2b3c3da7840b50bd4b3a7032c3192802bd4
Chirag Kumar <chiragkumar2199@gmail.com>
2025-04-10 11:06:26 +0530

    Initial commit - CloudQA Selenium Automation

---
+++ CloudQAAutomationTest/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System.Threading;
+
+namespace CloudQAAutomationTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://app.cloudqa.io/home/AutomationPracticeForm");
+
+            try
+            {
+                var firstNameField = driver.FindElement(By.Id("fname"));
+                firstNameField.SendKeys("Chirag");
+                var maleRadio = driver.FindElement(By.Id("male"));
+                maleRadio.Click();
+                var cricketCheckbox = driver.FindElement(By.Id("Cricket"));
+                cricketCheckbox.Click();
+                Thread.Sleep(3000);
+            }
+            catch (NoSuchElementException ex)
+            {
+                Console.WriteLine("Test failed: " + ex.Message);
+            }
+            finally
+            {
+                driver.Quit();
+            }
+        }
+    }
+}
